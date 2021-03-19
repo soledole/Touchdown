8cfdf89dcada373277f7105a42732571f9f16eb2
Jedrzej <jedrzej.kus@icloud.com>
2021-03-19 20:41:07 +0100

    Initial Commit

---
+++ Touchdown/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Touchdown
+//
+//  Created by Jędrzej Kuś on 19/03/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Jędrzej Kuś on 19/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
