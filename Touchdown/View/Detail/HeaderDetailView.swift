3fed622f56e9f329a0622fa7f8a84c02042b2ee4
Jedrzej <jedrzej.kus@icloud.com>
2021-03-24 09:59:24 +0100

    Finished App

---
+++ Touchdown/View/Detail/HeaderDetailView.swift
@@ -0,0 +1,40 @@
+//
+//  HeaderDetailView.swift
+//  Touchdown
+//
+//  Created by Jędrzej Kuś on 23/03/2021.
+//
+
+import SwiftUI
+
+struct HeaderDetailView: View {
+    
+    // MARK: - PROPERTIES
+    
+    @EnvironmentObject var shop: Shop
+    
+    // MARK: - BODY
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 6, content: {
+            Text("Protective Gear")
+            
+            Text(shop.selectedProduct?.name ?? sampleProduct.name)
+                .font(.largeTitle)
+                .fontWeight(.bold)
+        }) //: VSTACK
+        .foregroundColor(.white)
+    }
+}
+
+// MARK: - PREVIEW
+
+struct HeaderDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        HeaderDetailView()
+            .environmentObject(Shop())
+            .previewLayout(.sizeThatFits)
+            .padding()
+            .background(Color.gray)
+    }
+}
